4a2e69e7e4a61e28b90e96808c895e0a692e5550
aditighoshagd <aditighoshagd@gmail.com>
2026-07-27 05:11:36 +0530

    Add project configuration dependencies and Docker setup

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Build Stage
+FROM node:20-alpine AS builder
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+ARG VITE_API_BASE_URL=""
+ENV VITE_API_BASE_URL=$VITE_API_BASE_URL
+RUN npm run build
+
+# Runner Stage
+FROM node:20-alpine AS runner
+WORKDIR /app
+RUN npm install -g serve
+COPY --from=builder /app/dist ./dist
+
+ENV PORT=3000
+EXPOSE 3000
+
+CMD ["serve", "-s", "dist", "-l", "3000"]
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "::",
+    port: 5173,
+  },
+});
